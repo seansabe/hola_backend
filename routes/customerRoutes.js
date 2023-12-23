922f1f15c1f225a56cbd42bed29ea7b8368c05ec
Sergio Salazar <sa.sb@outlook.com>
2023-12-22 21:47:09 -0800

    route param id changes in customerRoutes.js

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 
 router.post('/', customerController.createCustomer);
-router.get('/:id', customerController.getCustomer);
+router.get('/:customerId', customerController.getCustomer);
 router.get('/', customerController.getCustomers);
-router.put('/:id/policy', customerController.updateCustomerPolicy);
-router.put('/:id', customerController.updateCustomer);
-router.delete('/:id', customerController.deleteCustomer);
+router.put('/:customerId/policy', customerController.updateCustomerPolicy);
+router.put('/:customerId', customerController.updateCustomer);
+router.delete('/:customerId', customerController.deleteCustomer);
 router.post('/delete', customerController.bulkDeleteCustomers);
 router.delete('/', customerController.deleteAllCustomers);
 
